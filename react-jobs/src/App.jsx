3d1f855b865e63cbeacd96da984f71ce0fd23a02
chibunduekwunife <chibunduekwunife@gmail.com>
2025-03-01 00:25:38 -0500

    CRUD functionality in progress

--- react-jobs/src/App.jsx
+++ react-jobs/src/App.jsx
@@ -10,22 +10,44 @@ import {
 import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
-
-const router = createBrowserRouter(
-
-  createRoutesFromElements(
-
-    // All children routes of the parent route inherit the MainLayout
-
-    <Route path='/' element={<MainLayout />} >
-      <Route index element={<HomePage />} />
-      <Route path='/jobs' element={<JobsPage />} />
-      <Route path='*' element={<NotFoundPage />} />
-    </Route>
-  )
-);
+import JobPage, { jobLoader } from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
 
 const App = () => {
+
+  // Add new JOB to the API
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob)
+    });
+    return;
+  }
+  
+  // Delete JOB from the API
+  const deleteJob = async (id) => {
+    console.log('delete', id)
+  }
+
+  const router = createBrowserRouter(
+  
+    createRoutesFromElements(
+  
+      // All children routes of the parent route inherit the MainLayout
+  
+      <Route path='/' element={<MainLayout />} >
+        <Route index element={<HomePage />} />
+        <Route path='/jobs' element={<JobsPage />} />
+        <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />}/>
+        <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob} />} loader={jobLoader} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Route>
+    )
+  );
+
   return <RouterProvider router={router} />;
 };
 
